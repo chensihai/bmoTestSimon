dbc58365aa69ac8d24ea55899060477d09311ea2
Simon Chen <csh@131676.no-reply.drupal.org>
2021-05-27 22:10:02 -0400

    Use fetch API instead of axios, avoid install extra code

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, setState } from 'react';
+import React, { useState, setState, setData } from 'react';
 
 
 class Search extends React.Component {
@@ -16,15 +16,38 @@ class Search extends React.Component {
         this.setState({ inputValue: e.target.value });
     }
 
-    searchFetch = (key) => {
-        debugger;
+    fetchBooksWithFetchAPI = (url) => {
+        this.setState({ ...this.state, isFetching: true });
+        fetch(url)
+            .then(response => response.json())
+            .then(result => {
+                this.setState({ Books: result.docs, isFetching: false })
+            })
+            .catch(e => {
+                console.log(e);
+                this.setState({ ...this.state, isFetching: false });
+            });
+        
+    };
+    
+    async searchFetch(key){
+
         console.log(key);
+        var fetchBooks = this.fetchBooksWithFetchAPI("http://openlibrary.org/search.json?q=" + key)
+  
+
+    }
+
+    Book() {
+        //this.state.Books.length
+        return ("test");
     }
     render() {
         return (<header className="App-header">
             Please type keywords to search your favoriate book
             <input value={this.state.inputValue} onChange={this.handleChange} onBlur={() => this.searchFetch(this.state.inputValue)} className="search_field" />
-        </header>);
+        </header>
+        );
 
     }
 
