776f67aa7fe405c8e2f28ac2465d18b1daef605f
Simon Chen <csh@131676.no-reply.drupal.org>
2021-05-27 20:16:02 -0400

    Split search to file..

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
+import Search from "./Search";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      Please type keywords to search your favoriate book
-         <input type="text" name="search" className="search_field" />
-      </header>
+    <Search />
     </div>
   );
 }
