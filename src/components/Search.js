160bd723fa770f6b9730afb026f91316bb9793b5
Simon Chen <csh@131676.no-reply.drupal.org>
2021-05-27 21:28:55 -0400

    Prepare search behavior, action only when blur

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState, setState } from 'react';
+
 
 class Search extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            inputValue: null
+        };
     }
 
+    componentDidMount() {
+        this.setState({ inputValue: this.props.inputValue });
+    }
+    handleChange = (e) => {
+        this.setState({ inputValue: e.target.value });
+    }
+
+    searchFetch = (key) => {
+        debugger;
+        console.log(key);
+    }
     render() {
         return (<header className="App-header">
             Please type keywords to search your favoriate book
-            <input type="text" name="search" className="search_field" />
+            <input value={this.state.inputValue} onChange={this.handleChange} onBlur={() => this.searchFetch(this.state.inputValue)} className="search_field" />
         </header>);
 
     }
